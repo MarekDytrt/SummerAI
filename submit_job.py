f4679d22af8d3973debfa2d59ce3c524fae3164c
Jakub Pekár <492788@mail.muni.cz>
2025-09-01 21:31:02 +0000

    feat: init

---
+++ submit_job.py
@@ -0,0 +1,4 @@
+# TODO: Create job submission script.
+# Read the docs at: https://gitlab.ics.muni.cz/rationai/infrastructure/kube-jobs/-/tree/summer-school
+# and based on the given instructions call the `submit_job` function.
+# Hint: you need to install the library either as project package and call it using `pdm` or as a cli client using pip
